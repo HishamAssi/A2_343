3d9ea945b578f343fbb7b38770f67fb70f06709d
Hisham El-Assi <hisham.el.assi@mail.utoronto.ca>
2017-11-03 14:35:40 -0400

    Should be done but must investigate a better way of doing it.

--- table_definitions/q1.sql
+++ table_definitions/q1.sql
@@ -12,13 +12,16 @@ voteRange VARCHAR(20),
 partyName VARCHAR(100)
 );
 
-
--- You may find it convenient to do this for each of the views
--- that define your intermediate steps.  (But give them better names!)
+-- Dropping the views that I'm about to create first just in case
 DROP VIEW IF EXISTS past_20 CASCADE;
 DROP VIEW IF EXISTS party_votes_ratios CASCADE;
-
--- Define views for your intermediate steps here.
+DROP VIEW IF EXISTS from0_5 CASCADE;
+DROP VIEW IF EXISTS from5_10 CASCADE;
+DROP VIEW IF EXISTS from10_20 CASCADE;
+DROP VIEW IF EXISTS from20_30 CASCADE;
+DROP VIEW IF EXISTS from30_40 CASCADE;
+DROP VIEW IF EXISTS from40 CASCADE;
+DROP VIEW IF EXISTS voteRanges CASCADE;
 
 -- Get a table of all the countries and parties that participated in an election that happened in the past 20 years.
 CREATE VIEW past_20 AS 
@@ -28,11 +31,53 @@ WHERE 1996 <= EXTRACT(YEAR FROM e_date) AND EXTRACT(YEAR FROM e_date) <= 2016;
 
 -- Get ratios.
 CREATE VIEW party_votes_ratios AS 
-SELECT year, country.name as countryName, (cast(votes as decimal) / votes_valid)*100 as voteRange, party.name as partyName
+SELECT year, country.name as countryName, (cast(votes as decimal) / cast(votes_valid as decimal))*100 as voteRatio, party.name as partyName
 FROM past_20 JOIN country ON country.id=country_id JOIN party ON party.id=party_id;
 
+-- For the next 6 views, I will be creating a different view for the different 
+-- ranges to include the ranges in the views.
+CREATE VIEW from0_5 AS 
+SELECT year, countryName, cast('(0-5]' as VARCHAR(20)) as voteRange, partyName
+FROM party_votes_ratios
+WHERE 0 < voteRatio AND voteRatio <= 5;
+
+CREATE VIEW from5_10 AS 
+SELECT year, countryName, cast('(5-10]' as VARCHAR(20)) as voteRange, partyName
+FROM party_votes_ratios
+WHERE 5 < voteRatio AND voteRatio <= 10;
+
+CREATE VIEW from10_20 AS 
+SELECT year, countryName, cast('(10-20]' as VARCHAR(20)) as voteRange, partyName
+FROM party_votes_ratios
+WHERE 10 < voteRatio AND voteRatio <= 20;
+
+CREATE VIEW from20_30 AS 
+SELECT year, countryName, cast('(20-30]' as VARCHAR(20)) as voteRange, partyName
+FROM party_votes_ratios
+WHERE 20 < voteRatio AND voteRatio <= 30;
+
+CREATE VIEW from30_40 AS 
+SELECT year, countryName, cast('(30-40]' as VARCHAR(20)) as voteRange, partyName
+FROM party_votes_ratios
+WHERE 30 < voteRatio AND voteRatio <= 40;
+
+CREATE VIEW from40 AS 
+SELECT year, countryName, cast('(40+]' as VARCHAR(20))  as voteRange, partyName
+FROM party_votes_ratios
+WHERE 40 < voteRatio ;
+
+-- Combining all the ranges together.
+CREATE VIEW allRanges AS
+SELECT * FROM (
+(SELECT * FROM from0_5) UNION
+(SELECT * FROM from5_10) UNION 
+(SELECT * FROM from10_20) UNION
+(SELECT * FROM from20_30) UNION
+(SELECT * FROM from30_40) UNION
+(SELECT * FROM from40)) AS from0_100;
 
 -- the answer to the query 
-insert into q1 
+insert into q1 (SELECT * FROM allRanges);
+
 
 
