32b06d85753d2ccc5683201ce0bc400201322877
Lana El Sanyoura <lana.elsanyoura@mail.utoronto.ca>
2017-11-08 15:03:06 -0500

    Implemented logic of q5

--- table_definitions/q5.sql
+++ table_definitions/q5.sql
@@ -33,24 +33,22 @@ WHERE b <= EXTRACT(YEAR FROM start_date) and EXTRACT(YEAR FROM start_date) <= e;
 -- Find the parties that are missing from the cabinet in the previous year
 
 -- Cabinets to previous
-CREATE VIEW  party_with_prev AS
-SELECT party_id, cabinet_party.cabinet_id, previous_cabinet_id, country_id, year
+CREATE VIEW  party_with_cab AS
+SELECT party_id, cabinet_party.cabinet_id, country_id
 FROM cabinet_party LEFT JOIN cab_within_20 ON cabinet_party.cabinet_id = cab_within_20.cabinet_id;
 
--- Best scenario is a party is in every cabinet
+-- Best scenario is a party is in every cabinet (perfect scenario)
 CREATE VIEW party_with_every_cabinet AS
 SELECT party_id, cabinet_id, country_id
-FROM cab_within_20 c1 JOIN cab_within_20 c2 ON c1.country_id = c2.country_id;
+FROM party JOIN cab_within_20 ON party.country_id = cab_within_20.country_id;
 
 -- Parties that have missed atleast one previous cabinet
-CREATE VIEW not_in_prev_cabinet AS
-SELECT party_id, cabinet_id, previous_cabinet, country_id, year
-FROM party_with_prev p1
-WHERE party_id NOT IN (
-	SELECT party_id, country_id
-	FROM party_with_prev p2
-	WHERE p1.previous_cabinet = p2.cabinet_id
-)
+CREATE VIEW rule_breakers AS
+(SELECT * FROM party_with_every_cabinet) EXCEPT (SELECT * FROM party_with_cab);
+
+-- Parties that have been with every cabinet in the past 20 years
+CREATE VIEW every_cabinet AS
+(SELECT party.id as party_id FROM party) EXCEPT (SELCET party_id FROM rule_breakers);
 
 -- the answer to the query 
 insert into q5 
