795e8275aecd5cc86a69f447849e9bad0d941b87
Lana El Sanyoura <lana.elsanyoura@mail.utoronto.ca>
2017-11-12 20:56:06 -0500

    Updated the query for part 2 q3

--- data/Assignment2.java
+++ data/Assignment2.java
@@ -56,7 +56,11 @@ public class Assignment2 extends JDBCSubmission {
 	int countryId = CountryId.getInt("id");
 	
 	
-        String queryElectionSequence = "(SELECT cabinet.id as cabinet_id, election as election_id FROM " +
+        String queryElectionSequence = "SELECT e.election_id as election_id, cabinet.id as cabinet_id " + 
+"FROM (SELECT e1.e_date as e_start, e2.e_date as e_end, e1.id as election_id, e1.country_id as country_id  FROM election e1 LEFT JOIN election e2 ON e1.e_type = e2.e_type AND e1.country_id = e2.country_id AND ((e1.id = e2.previous_parliament_election_id) OR (e1.id = e2.previous_ep_election_id))) AS e " +
+"JOIN cabinet ON cabinet.country_id = e.country_id WHERE ((cabinet.start_date >= e.e_start AND cabinet.start_date <= e.e_end) OR (cabinet.start_date >= e.e_start AND e.e_end is NULL )) AND (e.country_id = ?);";
+
+        /*"(SELECT cabinet.id as cabinet_id, election as election_id FROM " +
 	"(SELECT e2.id as election, e1.id as next, e2.e_date as s_date, e1.e_date as end_date, e1.country_id as c_id, e1.e_type as e_type " + 
 	"FROM election e1 JOIN election e2 ON e1.e_type = e2.e_type AND ((e2.id = e1.previous_parliament_election_id ) OR (e2.id = e1.previous_ep_election_id)) AND e1.country_id = e2.country_id WHERE e2.country_id = ?) AS election_cabinets " +
 "JOIN cabinet ON election_cabinets.c_id = cabinet.country_id " +
@@ -73,9 +77,9 @@ public class Assignment2 extends JDBCSubmission {
 	"FROM election " + 
 	"WHERE election.country_id = ? " +
 	"GROUP BY (e_type))) " +
-"AND (election.e_date <= cabinet.start_date)) " +*/
+"AND (election.e_date <= cabinet.start_date)) " +
 
-"ORDER BY election_id DESC;";
+"ORDER BY election_id DESC;";*/
 
 	PreparedStatement getElectionSequence = connection.prepareStatement(queryElectionSequence);
 	getElectionSequence.setInt(1,countryId);
