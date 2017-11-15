806f335539b06eb79cd5eed4897e2857ba8537cf
El Sanyoura Lana <elsanyo1@teach.cs.toronto.edu>
2017-11-14 22:17:23 -0500

    Adding test cases

---
+++ data/q1_test/q1.sql
@@ -0,0 +1,110 @@
+-- VoteRange
+
+SET SEARCH_PATH TO parlgov;
+drop table if exists q1 cascade;
+
+-- You must not change this table definition.
+
+create table q1(
+year INT,
+countryName VARCHAR(50),
+voteRange VARCHAR(20),
+partyName VARCHAR(100)
+);
+-- NO NULLS!!!
+-- Dropping the views that I'm about to create first just in case
+DROP VIEW IF EXISTS past_20 CASCADE;
+DROP VIEW IF EXISTS party_votes_ratios CASCADE;
+DROP VIEW IF EXISTS from0_5 CASCADE;
+DROP VIEW IF EXISTS from5_10 CASCADE;
+DROP VIEW IF EXISTS from10_20 CASCADE;
+DROP VIEW IF EXISTS from20_30 CASCADE;
+DROP VIEW IF EXISTS from30_40 CASCADE;
+DROP VIEW IF EXISTS from40 CASCADE;
+DROP VIEW IF EXISTS voteRanges CASCADE;
+
+-- Get a table of all the countries and parties that participated in an election that happened in the past 20 years.
+CREATE VIEW past_20 AS 
+SELECT EXTRACT(YEAR FROM e_date) as year, election.id as e_id, country_id, party_id, votes_valid, votes
+FROM election JOIN election_result ON election.id=election_id 
+WHERE 1996 <= EXTRACT(YEAR FROM e_date) AND EXTRACT(YEAR FROM e_date) <= 2016;
+
+-- TODO: check why the number of null values increases from past_20 to party_votes_ratios by 62.
+-- TODO: double check the average done in the 2nd view.
+
+-- Get ratios.
+CREATE VIEW party_votes_ratios AS 
+SELECT year, country.name as countryName, (cast(votes as decimal) / cast(votes_valid as decimal))*100 as voteRatio, party.name_short as partyName
+FROM past_20 JOIN country ON country.id=country_id JOIN party ON party.id=party_id;
+
+-- Average the ratios if more than one election occured in a year.
+CREATE VIEW avg_party_votes_ratios AS
+SELECT year, countryName, sum(voteRatio) / cast(count(*) as decimal) as voteRatio, partyName
+FROM party_votes_ratios
+GROUP BY year, partyName, countryName;
+
+
+-- For the next 6 views, I will be creating a different view for the different 
+-- ranges to include the ranges in the views.
+-- This view is for the range of 0 exclusive to 5 inclusive.
+CREATE VIEW from0_5 AS 
+SELECT year, countryName, cast('(0-5]' as VARCHAR(20)) as voteRange, partyName
+FROM avg_party_votes_ratios
+WHERE 0 < voteRatio AND voteRatio <= 5;
+
+-- This view is for the range of 5 exclusive to 10 inclusive.
+CREATE VIEW from5_10 AS 
+SELECT year, countryName, cast('(5-10]' as VARCHAR(20)) as voteRange, partyName
+FROM avg_party_votes_ratios
+WHERE 5 < voteRatio AND voteRatio <= 10;
+
+
+-- This view is for the range of 10 exclusive to 20 inclusive.
+CREATE VIEW from10_20 AS 
+SELECT year, countryName, cast('(10-20]' as VARCHAR(20)) as voteRange, partyName
+FROM avg_party_votes_ratios
+WHERE 10 < voteRatio AND voteRatio <= 20;
+
+
+-- This view is for the range of 20 exclusive to 30 inclusive.
+CREATE VIEW from20_30 AS 
+SELECT year, countryName, cast('(20-30]' as VARCHAR(20)) as voteRange, partyName
+FROM avg_party_votes_ratios
+WHERE 20 < voteRatio AND voteRatio <= 30;
+
+
+-- This view is for the range of 30 exclusive to 40 inclusive.
+CREATE VIEW from30_40 AS 
+SELECT year, countryName, cast('(30-40]' as VARCHAR(20)) as voteRange, partyName
+FROM avg_party_votes_ratios
+WHERE 30 < voteRatio AND voteRatio <= 40;
+
+-- This view is for the range of 40 onwards.
+CREATE VIEW from40 AS 
+SELECT year, countryName, cast('(40-100]' as VARCHAR(20))  as voteRange, partyName
+FROM avg_party_votes_ratios
+WHERE 40 < voteRatio;
+
+-- This view consists of all the parties that did not have a voteRange.
+CREATE VIEW null_parties AS 
+SELECT year, countryName, NULL  as voteRange, partyName
+FROM avg_party_votes_ratios
+WHERE voteRatio IS null;
+
+-- TODO :  null VOTES?
+
+-- Combining all the ranges together.
+CREATE VIEW allRanges AS
+SELECT * FROM (
+(SELECT * FROM from0_5) UNION
+(SELECT * FROM from5_10) UNION
+(SELECT * FROM from10_20) UNION
+(SELECT * FROM from20_30) UNION
+(SELECT * FROM from30_40) UNION
+(SELECT * FROM from40)) AS from0_100;
+
+-- the answer to the query
+insert into q1 (SELECT * FROM allRanges);
+
+
+
---
+++ data/q2_test/q2.sql
@@ -0,0 +1,104 @@
+-- Winners
+
+SET SEARCH_PATH TO parlgov;
+drop table if exists q2 cascade;
+
+-- You must not change this table definition.
+
+create table q2(
+countryName VARCHaR(100),
+partyName VARCHaR(100),
+partyFamily VARCHaR(100),
+wonElections INT,
+mostRecentlyWonElectionId INT,
+mostRecentlyWonElectionYear INT
+);
+
+
+-- You may find it convenient to do this for each of the views
+-- that define your intermediate steps.  (But give them better names!)
+DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS winnersByCountry  CASCADE;
+DROP VIEW IF EXISTS winnerCount CASCADE;
+DROP VIEW IF EXISTS losingParties CASCADE;
+DROP VIEW IF EXISTS winningParties1 CASCADE;
+DROP VIEW IF EXISTS winningParties CASCADE;
+DROP VIEW IF EXISTS winningPartiesWithVotes CASCADE;
+DROP VIEW IF EXISTS winnerCount CASCADE;
+DROP VIEW IF EXISTS averagePerCountry;
+DROP VIEW IF EXISTS eligibleParties;
+DROP VIEW IF EXISTS eligiblePartiesWithElections;
+DROP VIEW IF EXISTS mostRecentElection;
+
+
+-- Parties that did not win a specific election. 
+CREATE VIEW losingParties  AS 
+SELECT DISTINCT e1.election_id as election_id, e1.party_id as party_id 
+FROM election_result e1 JOIN election_result e2 ON 
+(e1.election_id = e2.election_id) and (e1.votes < e2.votes);
+
+-- Winning parties in an election.
+CREATE VIEW winningParties AS
+SELECT e1.election_id as election_id, e1.party_id as party_id 
+FROM election_result e1 INNER JOIN
+(SELECT election_id, max(votes) as votes 
+FROM election_result 
+GROUP BY election_id) e2
+ON e1.election_id = e2.election_id and e1.votes = e2.votes;
+
+-- Winning parties with the number of votes received.
+CREATE VIEW winningPartiesWithVotes AS
+SELECT winningParties.election_id as election_id, winningParties.party_id as party_id, votes, country_id
+FROM (winningParties JOIN election_result ON winningParties.party_id = election_result.party_id and winningParties.election_id = election_result.election_id) JOIN party ON winningParties.party_id = party.id;
+
+-- Number of wins for each party.
+CREATE VIEW winnerCount AS
+SELECT party_id, count(*) as wonElections, country_id 
+FROM winningParties JOIN party ON winningParties.party_id = party.id 
+GROUP BY party_id, country_id;
+
+-- Average number of wins for each country.
+CREATE VIEW averagePerCountry AS
+SELECT party.country_id, (cast(sum(wonElections) as decimal) / count(*)) as avg
+FROM party LEFT JOIN winnerCount ON party.id = party_id
+GROUP BY party.country_id;
+
+-- Parties with wins more than 3 times the average of their country's wins.
+CREATE VIEW eligibleParties AS
+SELECT party_id,wonElections,  averagePerCountry.avg
+FROM averagePerCountry JOIN winnerCount ON averagePerCountry.country_id = winnerCount.country_id
+WHERE wonElections > 3* averagePerCountry.avg;
+
+-- All eligible parties with all elections that they have won. 
+CREATE VIEW eligiblePartiesWithElections AS
+SELECT eligibleParties.party_id as party_id, wonElections,election_id, e_date 
+FROM (winningParties JOIN eligibleParties ON winningParties.party_id = eligibleParties.party_id) JOIN election ON id = election_id;
+
+-- All eligible parties with the most recent election won.
+CREATE VIEW mostRecentElection as
+SELECT e1.party_id as party_id, e1.wonElections as wonElections, e1.election_id as MostRecentlyWonElectionId, EXTRACT( YEAR from e1.e_date) as MostRecentlyWonElectionYear
+FROM eligiblePartiesWithElections e1 INNER JOIN
+(SELECT party_id, max(e_date) as e_date
+FROM eligiblePartiesWithElections 
+GROUP BY party_id) e2
+ON e1.party_id = e2.party_id and e1.e_date = e2.e_date;
+
+-- All eligible parties with needed information.
+CREATE VIEW eligible_names AS
+SELECT party.country_id, party.name as partyName,mostRecentElection.party_id,  wonElections, mostRecentlyWonElectionId, mostRecentlyWonElectionYear
+FROM mostRecentElection JOIN party ON party.id = party_id; 
+
+-- All eligible parties with even more information.
+CREATE VIEW eligible_countrynames AS
+SELECT country.name as countryName, partyName,party_id,  wonElections, mostRecentlyWonElectionId, mostRecentlyWonElectionYear
+FROM eligible_names  JOIN country ON eligible_names.country_id = country.id ;  
+
+-- All relevant information for all eligible parties as per the question specifications.
+CREATE VIEW eligible_familynames AS
+SELECT countryName, partyName, party_family.family as  partyFamily, wonElections, mostRecentlyWonElectionId, mostRecentlyWonElectionYear
+FROM eligible_countrynames LEFT JOIN party_family on eligible_countrynames.party_id = party_family.party_id ;  
+
+-- the answer to the query 
+insert into q2 (SELECT * FROM eligible_familynames);
+
+
